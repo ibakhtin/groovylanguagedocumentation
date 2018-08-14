f3546082ffc83444d5a3766a56afd5415b32c71e
ibakhtin <ibakhtin@icloud.com>
2018-08-14 20:36:07 +0300

    added RegularExpressionOperators.groovy

---
+++ src/main/groovy/groovylanguagespecification/operators/RegularExpressionOperators.groovy
@@ -0,0 +1,36 @@
+package groovylanguagespecification.operators
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+// Pattern operator
+
+def p = ~/foo/
+assert p instanceof Pattern
+
+//
+
+def pattern = 'bar'
+
+p = ~'foo'
+p = ~"foo"
+p = ~$/dollar/slashy $ string/$
+p = ~"${pattern}"
+
+// Find operator
+
+def text = "some text to match"
+def m = text =~ /match/
+assert m instanceof Matcher
+if (!m) {
+    throw new RuntimeException("Oops, text not found!")
+}
+
+//
+
+m = text ==~ /match/
+assert m instanceof Boolean
+if (m) {
+    throw new RuntimeException("Should not reach that point!")
+}
+
