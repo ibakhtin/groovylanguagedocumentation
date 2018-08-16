af3c77b8214707f01519e5c61c04f7ae0381deb6
ibakhtin <ibakhtin@icloud.com>
2018-08-16 08:44:18 +0300

    updated Traits.groovy

--- src/main/groovy/groovylanguagespecification/objectorientation/Traits.groovy
+++ src/main/groovy/groovylanguagespecification/objectorientation/Traits.groovy
@@ -1,2 +1,38 @@
 package groovylanguagespecification.objectorientation
 
+trait FlyingAbility {
+    String fly() { "I'm flying!" }
+}
+
+//
+
+class Bird implements FlyingAbility {}
+def b = new Bird()
+assert b.fly() == "I'm flying!"
+
+// Methods
+
+// Public methods
+
+trait SwimmingAbility {
+    String swim() { "I'm swimming!" }
+}
+
+// Abstract methods
+
+trait Greetable {
+    abstract String name()
+    String greeting() { "Hello, ${name()}!" }
+}
+
+//
+
+class Person9 implements Greetable {
+    String name() { 'Bob' }
+}
+
+def person9 = new Person9()
+assert person9.greeting() == 'Hello, Bob!'
+
+// Private methods
+
