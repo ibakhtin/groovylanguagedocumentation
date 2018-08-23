ff15bbd19f66af278b9261b19d5b634d91876095
ibakhtin <ibakhtin@icloud.com>
2018-08-23 17:26:08 +0300

    updated WorkingWithDateTimeTypes.groovy

--- src/main/groovy/userguides/groovydevelopmentkit/WorkingWithDateTimeTypes.groovy
+++ src/main/groovy/userguides/groovydevelopmentkit/WorkingWithDateTimeTypes.groovy
@@ -4,7 +4,9 @@ import java.time.LocalDate
 import java.time.LocalTime
 import java.time.Month
 import java.time.OffsetTime
+import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
 
 // Formatting and parsing
 
@@ -16,3 +18,15 @@ assert time == LocalTime.of(4, 45, 0)
 
 def offsetTime = OffsetTime.parse('09:47:51-1234', 'HH:mm:ssZ')
 assert offsetTime == OffsetTime.of(9, 47, 51, 0, ZoneOffset.ofHoursMinutes(-12, -34))
+
+def dateTime = ZonedDateTime.parse('2017/07/11 9:47PM Pacific Standard Time', 'yyyy/MM/dd h:mma zzzz')
+assert dateTime == ZonedDateTime.of(
+        LocalDate.of(2017, 7, 11),
+        LocalTime.of(21, 47, 0),
+        ZoneId.of('America/Los_Angeles')
+)
+
+// Manipulating date/time
+
+//// Addition and subtraction
+
